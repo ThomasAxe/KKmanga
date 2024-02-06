33fa0fcf824d17db52161e09ce0b987cd13b9ea3
ThomasAxe <tomas.rvc.machado@gmail.com>
2024-02-06 15:52:50 +0000

    bg and comments

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -4,12 +4,21 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <!-- header == navbar -->
   <header>
+    <!--div == home logo / anchors-->
+    <div>
+
+    </div>
     
   </header>
 
+  <!-- main == body -->
   <main>
     
   </main>
 </template>
+<style scoped>
+      
+</style>
 
